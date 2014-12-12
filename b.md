f6dd0d3b26d0983d5e665d939df942d9a965cfc1
Pat Cavit <pcavit@gmail.com>
2014-12-11 22:23:03 -0800

    Showing some useful output now

--- b.md
+++ b.md
@@ -8,10 +8,10 @@ Content B Content B Content B Content B Content B Content B Content B Content B 
 Content B Content B Content B Content B Content B Content B
 
 * Content B
-* Content B
-* Content B
-* Content B
-* Content B
+* Content C
+* Content D
+* Content E
+* Content F
 
 Content B Content B Content B Content B Content B Content B
 
@@ -21,6 +21,6 @@ Content B Content B Content B Content B Content B Content B
 1. Content B
 1. Content B
 
-[Link B](/link-B/link-B/link-B)
+[Link B](/link-B)
 [Link B](/link-B/link-B/link-B)
 [Link B](/link-B/link-B/link-B)
--- index.js
+++ index.js
@@ -3,27 +3,54 @@
 var fs = require("fs"),
     Remarkable = require("remarkable"),
     md = new Remarkable(),
-    diff = require("deep-diff").diff,
+    deepDiff = require("deep-diff").diff,
+    diff = require("diff"),
+    chalk = require("chalk"),
     obj = require("object-path");
     
 var a = fs.readFileSync("./a.md", "utf8");
 var b = fs.readFileSync("./b.md", "utf8");
 
 var template = md.parse(a, {});
+var compare  = md.parse(b, {});
 
-var compare = md.parse(b, {});
-
-
-var changes = diff(template, compare);
-
-changes = changes.filter(function(change) {
-    return change.path[change.path.length - 1] !== "content";
-});
-
-console.log(template);
+var changes = deepDiff(template, compare),
+    lines   = {};
 
 changes.forEach(function(change) {
-    var path = change.path.slice(0, -1);
+    var loc;
     
-    console.log(obj.get(template, path.join(".")));
+    if(!change.path || change.path[change.path.length - 1] === "content") {
+        return;
+    }
+    
+    change.path.some(function(part, idx) {
+        loc = change.path.slice(0, idx + 1).join(".");
+        
+        return !!obj.get(template, loc).lines;
+    });
+    
+    lines[obj.get(template, loc).lines.join(",")] = true;
+});
+
+var al = a.split(/\r?\n/g),
+    bl = b.split(/\r?\n/g);
+
+Object.keys(lines).forEach(function(lines) {
+    var parts;
+    
+    lines = lines.split(",");
+    
+    parts = diff.diffWords(
+        al.slice(lines[0], lines[1]).join("\n"),
+        bl.slice(lines[0], lines[1]).join("\n")
+    );
+    
+    parts.forEach(function(part) {
+        process.stderr.write(
+            part.removed || part.added ?
+                chalk["bg" + (part.added ? "Green" : "Red")](part.value) :
+                chalk.dim(part.value)
+        );
+    });
 });
